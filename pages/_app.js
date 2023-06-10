c414c2e73d1e3a5ef2eeb0bc0761fbdf9e135aaf
Denis <41701228+danmiles@users.noreply.github.com>
2023-06-11 01:53:36 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
       <title>Metaversus</title>
+      <meta name="description" content="Next.js 13 APP - Tailwind CSS and Animation framer-motion" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
